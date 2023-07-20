1bb8b152339d480b279dbe151cb5333a6708594f
superfishal <97133716+superfishal@users.noreply.github.com>
2023-07-20 15:05:40 -0700

    header and body and footer

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        height: "1000px",
+      }}
+    >
+      this is page.tsx
+    </div>
+  );
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Header.module.css";
+import Image from "next/image";
+import logo from "./images/superfishal_logo.jpg";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.menu}>
+        <div className={styles.logo}>
+          <Image fill src={logo} alt="cool logo" />
+        </div>
+        <ul className={styles.navLinks}>
+          <li>link1</li>
+          <li>link2</li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
